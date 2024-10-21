1511054a66d3f09a1760ba3bb71ab516fe7b1df9
tunafysh <hanansmani@gmail.com>
2024-10-21 13:35:37 +0100

    did some shi-

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,72 +1,85 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-
+import React, { useEffect, useRef } from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Animated } from 'react-native';
+import { SlideInUp, SlideOutDown } from 'react-native-reanimated';
 
 export default function TabLayout() {
+  const animation = useRef(new Animated.Value(-100)).current;
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   return (
-    <Tabs
-      screenOptions={{
-        tabBarLabelStyle: { display: 'none' },
-        tabBarStyle: {
-          elevation: 0,
-          position: "absolute",
-          backgroundColor: "#030707",
-          borderColor: '#199A93', 
-          borderTopWidth: 3,
-          borderLeftWidth: 3,
-          borderRightWidth: 3,
-          height: 60,
-          borderTopStartRadius: 16,
-          borderTopEndRadius: 16
-        },
-        tabBarActiveTintColor: "#199A93",
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: undefined,
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon size={30} name={focused ? 'home' : 'home-outline'} color="#199A93" />
-          ),
+    <Animated.View style={{ transform: [{ translateY: animation }] }}>
+      <Tabs
+        screenOptions={{
+          tabBarLabelStyle: { display: 'none' },
+          tabBarStyle: {
+            elevation: 0,
+            position: 'absolute',
+            backgroundColor: '#030707',
+            borderColor: '#199A93',
+            borderTopWidth: 3,
+            borderLeftWidth: 3,
+            borderRightWidth: 3,
+            height: 60,
+            borderTopStartRadius: 16,
+            borderTopEndRadius: 16,
+          },
+          tabBarActiveTintColor: '#199A93',
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="create"
-        options={{
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon size={30} name={focused ? 'search-circle' : 'search-circle-outline'} color="#199A93" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="notifications"
-        options={{
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon size={30} name="add-outline" color="#199A93" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="friends"
-        options={{
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon size={30} name={focused ? 'home' : 'home-outline'} color="#199A93" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon size={30} name={focused ? 'code-slash' : 'code-slash-outline'} color="#199A93" />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: undefined,
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon size={30} name={focused ? 'home' : 'home-outline'} color="#199A93" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon size={30} name={focused ? 'search-circle' : 'search-circle-outline'} color="#199A93" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="notifications"
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon size={30} name="add-outline" color="#199A93" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="friends"
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon size={30} name={focused ? 'home' : 'home-outline'} color="#199A93" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon size={30} name={focused ? 'code-slash' : 'code-slash-outline'} color="#199A93" />
+            ),
+          }}
+        />
+      </Tabs>
+    </Animated.View>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
-import { Image, StyleSheet, Platform, Text, useColorScheme } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Image, StyleSheet, Platform, Text, useColorScheme, Animated } from 'react-native';
 import { ScrollView } from 'react-native';
+import { useAnimatedStyle } from 'react-native-reanimated';
 export default function HomeScreen() {
 
   const colorScheme = useColorScheme();
@@ -15,13 +17,25 @@ const style = StyleSheet.create({
   titleBarStyle: {
     fontWeight: '900',
     fontSize: 32,
+    opacity: 0,
     color: colorScheme == "dark"? "#EEF6F6": ""
   }
 })
+  var fadeAnim = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: true,
+      }
+    ).start();
+  }, [fadeAnim]);
 
   return (
     <ScrollView style={style.homeScreenStyle}>
-      <Text style={style.titleBarStyle}>Welcome <Text style={{color: '#199A93'}}>Hanan</Text></Text>
+      <Animated.Text  style={[style.titleBarStyle, {opacity: fadeAnim}]}>Welcome <Text style={{color: '#199A93'}}>Hanan</Text></Animated.Text>
     </ScrollView>   
   );
 }
