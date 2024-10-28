c62fe2ee5b100da08e5b68e25dd2255d22d39b70
tunafysh <hanansmani@gmail.com>
2024-10-28 20:12:22 +0100

    restarting from tauri

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Pulsewave
 
-### Description
-A mock social media app just for demo and maybe just to prove i can build an app with react native even though i never tried it in my life.
+A lackluster social media app.
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// import { ModeToggle } from "@/components/themetoggle";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-background flex flex-col w-screen">
+      <div className="w-full bg-background h-full" />
+      <div id="navbar" className="absolute flex items-center justify-evenly w-full bg-background h-[6%] bottom-0 z-10 border-t-4 border-x-4 border-primary rounded-t-xl">
+      </div>
+    </div>
+  );
+}
